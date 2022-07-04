d525ae8c2e9c9da2fff047607b851c011592bb94
Mohammad Ahmadi <webdev.ahmadi@gmail.com>
2022-07-04 17:03:15 +0430

    Make seperate file RabbitMqConnection.cs

---
+++ src/RabbitMqClient.Core/Messaging/RabbitMqConnection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+
+namespace RabbitMqClient.Core.Messaging
+{
+    public class RabbitMqConnection : IRabbitMqConnection
+    {
+        private readonly IConfiguration _configuration;
+
+        public RabbitMqConnection(IConfiguration configuration)
+        {
+            _configuration = configuration
+                ?? throw new ArgumentNullException(nameof(IConfiguration));
+        }
+
+        public IConnection GetConnection(string connectionStringName = "RabbitMq")
+        {
+            var connectionString = _configuration.GetConnectionString(connectionStringName);
+            if (string.IsNullOrEmpty(connectionString))
+                throw new NullReferenceException("RabbitMq connection string is null or empty!");
+
+            var factory = new ConnectionFactory { Uri = new Uri(connectionString) };
+            return factory.CreateConnection();
+        }
+    }
+}
